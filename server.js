9494bc5065dae11cb925718d95dac8730a4180bc
Andrew Sturman <andrewdavidsturman@gmail.com>
2023-08-17 11:51:28 +1000

    Added connect retry loop for database connection
    The MySQL local development container takes a while to fully start.
    When the backend and database together with docker compose, the
    backend would come online first, and fail it's connection to the
    database.
    
    Lets add a configurable connect retry loop to the backend to
    check multiple times if the database is available, before shutting
    down the server.

--- server.js
+++ server.js
@@ -2,24 +2,39 @@ require('dotenv').config();
 const db = require('./config/db');
 const express = require('./config/express');
 
+const maxDbConnectionAttempts = process.env.MAX_DB_CONNECTION_ATTEMPTS ?? 5;
+let remainingDbConnectionAttempts = maxDbConnectionAttempts;
+
+const dbConnectionReattemptBackoffTimeSeconds = process.env.DB_CONNECTION_REATTEMPT_BACKOFF_TIME_SECONDS ?? 15
 
 const app = express();
 const port = process.env.PORT || 4941;
 
 // Test connection to MySQL on start-up
 async function testDbConnection() {
-    try {
         await db.createPool();
         await db.getPool().getConnection();
-    } catch (err) {
-        console.error(`Unable to connect to MySQL: ${err.message}`);
-        process.exit(1);
-    }
 }
 
-testDbConnection()
-    .then(function () {
-        app.listen(port, function () {
-            console.log(`Listening on port: ${port}`);
-        });
-    });
+function startServer() {
+    testDbConnection()
+    .then(() => app.listen(port, () => console.log(`Listening on port: ${port}`)))
+    .catch(err => {
+        console.log(`Unnable to connect to MySQL on attempt ${maxDbConnectionAttempts - remainingDbConnectionAttempts + 1}/${maxDbConnectionAttempts})`);
+        console.log(`Error: ${err.message}`);
+        remainingDbConnectionAttempts -= 1;
+        if (remainingDbConnectionAttempts > 0) {
+            console.log(`Retrying connection in ${dbConnectionReattemptBackoffTimeSeconds}s`)
+            setTimeout(startServer, dbConnectionReattemptBackoffTimeSeconds * 1000);
+        } else {
+            console.log(`Could not establish a database connection after ${maxDbConnectionAttempts} attempts. Shutting down.`)
+            process.exit(1)
+        }
+    })
+}
+
+startServer()
+
+
+
+    
